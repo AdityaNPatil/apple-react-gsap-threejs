7b3b29e6052ee3ee83087400a3bfe9609559084f
AdityaNPatil <adityapatil2708@gmail.com>
2024-12-14 23:13:47 +0530

    gsap.to method

--- GSAP WORKSHOP/gsap-cc-starter-main/src/pages/GsapTo.jsx
+++ GSAP WORKSHOP/gsap-cc-starter-main/src/pages/GsapTo.jsx
@@ -1,8 +1,31 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// ◘◘ import gsap ,,,, useGSAP from @gsap/react 
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
 
+  // ♦ useGSAP is similar to useLayoutEffect(fallback of useEffect) hook of react -- it also handles cleanup of gsap  objects 
+  useGSAP(()=>{
+    gsap.to("#blue-box" , {
+      x: 500,
+      rotation: 180,
+      ease: "power1", // how animation happens
+      duration: 1,    // duration of animation  
+      repeat: -1,     // -1 means keep repeating infinitely
+      yoyo: true      // means keep going in alternate directions
+    }) // gsap.to(target , vars)
+
+    // gsap.to returns a tween
+  } , []) // dependency array similar to useEffect
+
   return (
     <main>
+      <Link to={"/"}><small>Home</small></Link>
+
       <h1>GsapTo</h1>
 
       <p className="mt-5 text-gray-500">
@@ -29,7 +52,7 @@ const GsapTo = () => {
         method.
       </p>
 
-      {/* image to animate */}
+      {/* image to animate  */}
       <div className="mt-20">
         <div id="blue-box" className="w-20 h-20 bg-blue-500 rounded-lg" />
       </div>

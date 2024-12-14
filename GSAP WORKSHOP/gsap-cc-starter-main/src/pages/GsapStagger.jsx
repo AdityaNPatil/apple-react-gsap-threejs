aea97a55818b2bcf8057dc8856919aff3594c5ad
AdityaNPatil <adityapatil2708@gmail.com>
2024-12-15 00:31:39 +0530

    stagger property

--- GSAP WORKSHOP/gsap-cc-starter-main/src/pages/GsapStagger.jsx
+++ GSAP WORKSHOP/gsap-cc-starter-main/src/pages/GsapStagger.jsx
@@ -1,8 +1,49 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Link } from "react-router-dom";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
 
+  // ADVANCED CONFIG for stagger
+  /*
+    gsap.to('.box', {
+      y: 100,
+      stagger: {
+          // wrap advanced options in an object
+          each: 0.1,
+          from: 'center',
+          grid: 'auto',
+          ease: 'power2.inOut',
+          repeat: -1 // Repeats immediately, not waiting for the other staggered animations to finish
+      }
+    });
+  */
+
+  useGSAP(()=>{
+    gsap.to(".stagger-box",{
+      y:250,
+      rotaion:360,
+      borderRadius:"100%",
+      repeat: -1,
+      duration:1,
+      yoyo: true,
+      // stagger property -- animate each object 1 by 1 
+      stagger: {
+        amount: 0.8,  // The total amount of time (in seconds) that gets split among all the staggers
+        grid: [3,1],  // number of rows and cols visually for the elements
+        axis: "y",    // If you define a grid, staggers are based on each element's total distance to the "from" value on both the x and y axis, but you can focus on just one axis if you prefer  
+        // here everything is happening in y axis - hence we give axis as y
+        ease: "power1.inOut",
+        from: "center"  // from where staggered animation starts -- "start", "center", "edges", "random", or "end"
+      }
+    })
+  })
+
   return (
     <main>
+      <Link to={"/"}> <small>Home</small> </Link>
+
       <h1>GsapStagger</h1>
 
       <p className="mt-5 text-gray-500">

0bd8f74e8e51dbe83b34e7881647a43ef1d88b4c
AdityaNPatil <adityapatil2708@gmail.com>
2024-12-15 01:23:38 +0530

    text animation using gsap

--- GSAP WORKSHOP/gsap-cc-starter-main/src/pages/GsapText.jsx
+++ GSAP WORKSHOP/gsap-cc-starter-main/src/pages/GsapText.jsx
@@ -1,12 +1,61 @@
+import { Link } from "react-router-dom";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapText = () => {
   // TODO: Implement gsap text animation
 
+  /*
+    gsap.to(yourElement, {
+      duration: 1,
+      text: {
+        value: "Your new text",
+        newClass: "class2",
+        delimiter: " ",
+      },
+    });
+  */
+
+  useGSAP(()=>{
+    // heading animate
+    gsap.to("#text" , {
+      duration:2,
+      ease:"power.inOut",
+      opacity:1,
+      y:0,
+      x: "45%"
+    })
+
+    // paragraph animate
+    gsap.fromTo(".para",
+      // from
+      {
+        opacity:0,
+        y:20
+      },
+      // to
+      {
+        opacity:1,
+        y:0,
+        delay: 0.3,
+        stagger:{
+          amount:1
+        }
+      }
+    )
+  } , [])
+
   return (
     <main>
+      <Link to={"/"}> <small>Home</small> </Link>
+
+      {/* apply text animation here */}
       <h1 id="text" className="opacity-0 translate-y-10">
         GsapText
       </h1>
 
+      {/* apply text animation to paragraphs */}
       <p className="mt-5 text-gray-500 para">
         We can use same method like <code>gsap.to()</code>,{" "}
         <code>gsap.from()</code>, <code>gsap.fromTo()</code> and{" "}

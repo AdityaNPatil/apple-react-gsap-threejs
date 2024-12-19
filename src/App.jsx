d726e96e265711c9b5376d1778da566efeea1364
AdityaNPatil <adityapatil2708@gmail.com>
2024-12-20 00:31:30 +0530

    Features section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import Features from "./components/Features"
 
 // Import Sentry
 import * as Sentry from "@sentry/react";
@@ -15,6 +16,7 @@ function App() {
       <Hero/>
       <Highlights/>
       <Model/>
+      <Features/>
     </main>
   )
 }
